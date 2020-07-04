8edc42c1e12040216b1b1482004e2c9aa4c75369
piaoliangkb <piaoliangkb@gmail.com>
2020-07-04 22:00:01 +0800

    Initialize local dotfiles on macos

---
+++ .hammerspoon/init.lua
@@ -0,0 +1,12 @@
+require('util')
+
+-- move window to anywhere
+-- see more info in window.lua
+require('window')
+
+-- auto change input method in different app
+-- see current app dir and input method using ctrl+command+.
+require('ime')
+
+-- use command+up/down to set volume
+require('volume')
