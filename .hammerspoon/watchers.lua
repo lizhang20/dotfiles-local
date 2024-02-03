4a3c427bab8df130e2e29b3354f7eb7afe607396
piaoliangkb <piaoliangkb@gmail.com>
2024-02-03 09:49:03 +0800

    add screen_unlock watcher

--- .hammerspoon/watchers.lua
+++ .hammerspoon/watchers.lua
@@ -3,26 +3,24 @@ require('volume')
 logger = hs.logger.new("watchers", "info")
 
 function netAuth()
-  _, status, _ = hs.execute("ifconfig en7")
-  if status then
-    logger.i("network interface en7 detected")
-    output, _, _ = hs.execute("pwd")
-    logger.i("current working dir " .. output)
-    -- execute a script in current directory: net-auth.sh
-    output, status, _ = hs.execute("bash ./net-auth.sh")
-    logger.i("netwotk auth result " .. output)
-    if not status then
-        hs.execute("bash ./net-auth.sh")
+    _, status, _ = hs.execute("ifconfig en7")
+    if status then
+        logger.i("network interface en7 detected")
+        output, _, _ = hs.execute("pwd")
+        logger.i("current working dir " .. output)
+        -- execute a script in current directory: net-auth.sh
+        output, status, _ = hs.execute("bash ./net-auth.sh")
+        logger.i("netwotk auth result " .. output)
+        if not status then hs.execute("bash ./net-auth.sh") end
     end
-  end
 end
 
 wakeupWatcher = hs.caffeinate.watcher.new(function(state)
-  if state == hs.caffeinate.watcher.systemDidWake then
-    -- mute the volume
-    muteBuiltInSpeakerDevice()
-
-    -- network authentication when there is en7
-    netAuth()
-  end
+    if (state == hs.caffeinate.watcher.systemDidWake or 
+        state == hs.caffeinate.watcher.screensDidUnlock) then
+        -- mute the volume
+        muteBuiltInSpeakerDevice()
+        -- network authentication when there is en7
+        netAuth()
+    end
 end):start()
